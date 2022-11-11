a26f6c7e796f3c91a0a22945c6def92a6dc87c97
Kaya <belkaya@emea.corpdir.net>
2022-11-11 14:43:34 +0100

    refactoring the code

---
+++ project_config.py
@@ -0,0 +1,7 @@
+# defined page to retrieve the minute traffic data information
+page_url ='https://datos.madrid.es/sites/v/index.jsp?vgnextoid=33cb30c367e78410VgnVCM1000000b205a0aRCRD&vgnextchannel=374512b9ace9f310VgnVCM100000171f5a0aRCRD'
+base_url= 'https://datos.madrid.es/egob/catalogo/'
+
+# chunk size in parallel processing
+chunk_size = 50000
+
